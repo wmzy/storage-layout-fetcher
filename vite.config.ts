6f90587a9cedc3a5a2d5685572d25d9a507d7490
wmzy <1256573276@qq.com>
2025-03-07 11:44:02 +0800

    ci: fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import { resolve } from 'path';
 
 export default defineConfig({
   build: {
+    target: 'node18',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'storage-layout-fetcher',
       formats: ['es', 'cjs'],
       fileName: (format) => `index.${format}.js`,
     },
+    minify: false,
     sourcemap: true,
+    rollupOptions: {
+      external: /^[^\/\.].*/,
+    },
   },
   resolve: {
     alias: {
